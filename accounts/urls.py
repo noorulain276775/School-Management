94782580d6b9683cbc8048329bbb7b08966c6bde
Noor ul ain Ibrahim <noor.ibrahim@softversum.com>
2023-10-15 15:43:47 +0400

    viewsets added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
-# from django.contrib.auth.decorators import user_passes_test
-
-# @user_passes_test(lambda user: user.user_type == 'admin')
-# def admin_only_view(request):
-    # Your view logic here
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
